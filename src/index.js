5686cf74cd66d3e4c5b5c591ed164b1c7b654328
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-05-01 02:04:30 +0300

    add data file.json

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from './data/user.json';
+import statistics from './data/statistics.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App data={{ user, statistics }} />
   </React.StrictMode>
 );
