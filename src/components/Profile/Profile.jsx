ad3e800597b2884136e9429b2f5efff955acd0b8
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-05-01 02:08:04 +0300

    edit

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,43 +1,44 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
 
-// const Profile = ({ username, tag, location, avatar, stats }) => (
-//     <div className={profile}>
-//         <div className={description}>
-//             <img src={avatar} alt="User avatar" className={avatar} />
-//             <p className={name}>{username}</p>
-//             <p className={tag}>@{tag}</p>
-//             <p className={location}>{location}</p>
-//         </div>
+const Profile = ({ username, tag, location, avatar, stats }) => (
+  <div className={s.profile}>
+    <div className={s.description}>
+      <img src={avatar} alt="User avatar" className={s.avatar} />
+      <p className={s.name}>{username}</p>
+      <p className={s.tag}>@{tag}</p>
+      <p className={s.location}>{location}</p>
+    </div>
 
-//         <ul className={stats}>
-//             <li>
-//                 <span className={label}>Followers</span>
-//                 <span className={quantity}>{followers}</span>
-//             </li>
+    <ul className={s.stats}>
+      <li>
+        <span className={s.label}>Followers</span>
+        <span className={s.quantity}>{stats.followers}</span>
+      </li>
 
-//             <li>
-//                 <span className={label}>Views</span>
-//                 <span className={quantity}>{views}</span>
-//             </li>
+      <li>
+        <span className={s.label}>Views</span>
+        <span className={s.quantity}>{stats.views}</span>
+      </li>
 
-//             <li>
-//                 <span className={label}>Likes</span>
-//                 <span className={quantity}>{likes}</span>
-//             </li>
-//         </ul>
-//     </div>
-// );
+      <li>
+        <span className={s.label}>Likes</span>
+        <span className={s.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
 
-// Profile.propTypes = {
-//     username: PropTypes.string.isRequired,
-//     tag: PropTypes.string.isRequired,
-//     location: PropTypes.string.isRequired,
-//     avatar: PropTypes.string.isRequired,
-//     stats: PropTypes.shape({
-//         followers: PropTypes.number.isRequired,
-//         views: PropTypes.number.isRequired,
-//         likes: PropTypes.number.isRequired,
-//     }),
-// };
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
 
-// export default Profile;
+export default Profile;
